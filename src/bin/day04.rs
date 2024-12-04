3cdf97679f20d76003ceacb6cb5b996596044961
Adam Vandolder <adam.vandolder@gmail.com>
2024-12-04 01:22:00 -0500

    Reduce integer type conversions for day 4

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -7,7 +7,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .lines()
         .map(|line| line.chars().collect::<Vec<_>>())
         .collect::<Vec<_>>();
-    let dimensions = (grid.len() as i64, grid[0].len() as i64);
+    let dimensions = (grid.len(), grid[0].len());
 
     println!("part one: {}", part_one(&grid, dimensions));
     println!("part two: {}", part_two(&grid, dimensions));
@@ -29,9 +29,9 @@ fn search_for(
         == target
 }
 
-fn part_one(grid: &[Vec<char>], (rows, cols): (i64, i64)) -> i64 {
+fn part_one(grid: &[Vec<char>], (rows, cols): (usize, usize)) -> usize {
     iproduct!(0..rows, 0..cols)
-        .filter(|(row, col)| grid[*row as usize][*col as usize] == 'X')
+        .filter(|(row, col)| grid[*row][*col] == 'X')
         .map(|(row, col)| {
             [
                 (0, 1),
@@ -44,20 +44,20 @@ fn part_one(grid: &[Vec<char>], (rows, cols): (i64, i64)) -> i64 {
                 (-1, -1),
             ]
             .into_iter()
-            .filter(|direction| search_for(grid, (row, col), *direction, "XMAS"))
-            .count() as i64
+            .filter(|direction| search_for(grid, (row as i64, col as i64), *direction, "XMAS"))
+            .count()
         })
         .sum()
 }
 
-fn part_two(grid: &[Vec<char>], (rows, cols): (i64, i64)) -> i64 {
+fn part_two(grid: &[Vec<char>], (rows, cols): (usize, usize)) -> usize {
     iproduct!(0..rows, 0..cols)
         .filter(|(row, col)| {
-            let (row, col) = (*row, *col);
+            let (row, col) = (*row as i64, *col as i64);
             (search_for(grid, (row, col), (1, 1), "MAS")
                 || search_for(grid, (row, col), (1, 1), "SAM"))
                 && (search_for(grid, (row + 2, col), (-1, 1), "MAS")
                     || search_for(grid, (row + 2, col), (-1, 1), "SAM"))
         })
-        .count() as i64
+        .count()
 }
