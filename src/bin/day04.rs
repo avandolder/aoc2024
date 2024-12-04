c86f84a2f46b0917a291cca0a570f68f0b95c052
Adam Vandolder <adam.vandolder@gmail.com>
2024-12-04 01:15:07 -0500

    Remove redundancies from day 4 solution

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -3,35 +3,34 @@ use itertools::iproduct;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = aoc2024::read_input()?;
 
-    println!("part one: {}", part_one(&input));
-    println!("part two: {}", part_two(&input));
-
-    Ok(())
-}
-
-fn search_for(
-    grid: &[Vec<char>],
-    (rows, cols): (i64, i64),
-    (row, col): (i64, i64),
-    direction: (i64, i64),
-    target: &'static str,
-) -> bool {
-    (0..target.len() as i64)
-        .map(|idx| (row + direction.0 * idx, col + direction.1 * idx))
-        .filter(|(row, col)| *row >= 0 && *row < rows && *col >= 0 && *col < cols)
-        .map(|(row, col)| grid[row as usize][col as usize])
-        .collect::<String>()
-        == target
-}
-
-fn part_one(input: &str) -> i64 {
     let grid = input
         .lines()
         .map(|line| line.chars().collect::<Vec<_>>())
         .collect::<Vec<_>>();
     let dimensions = (grid.len() as i64, grid[0].len() as i64);
 
-    iproduct!(0..dimensions.0, 0..dimensions.1)
+    println!("part one: {}", part_one(&grid, dimensions));
+    println!("part two: {}", part_two(&grid, dimensions));
+
+    Ok(())
+}
+
+fn search_for(
+    grid: &[Vec<char>],
+    (row, col): (i64, i64),
+    direction: (i64, i64),
+    target: &str,
+) -> bool {
+    (0..target.len() as i64)
+        .map(|idx| (row + direction.0 * idx, col + direction.1 * idx))
+        .filter(|(row, col)| *row >= 0 && *col >= 0)
+        .filter_map(|(row, col)| grid.get(row as usize).and_then(|row| row.get(col as usize)))
+        .collect::<String>()
+        == target
+}
+
+fn part_one(grid: &[Vec<char>], (rows, cols): (i64, i64)) -> i64 {
+    iproduct!(0..rows, 0..cols)
         .filter(|(row, col)| grid[*row as usize][*col as usize] == 'X')
         .map(|(row, col)| {
             [
@@ -45,26 +44,20 @@ fn part_one(input: &str) -> i64 {
                 (-1, -1),
             ]
             .into_iter()
-            .filter(|direction| search_for(&grid, dimensions, (row, col), *direction, "XMAS"))
+            .filter(|direction| search_for(grid, (row, col), *direction, "XMAS"))
             .count() as i64
         })
         .sum()
 }
 
-fn part_two(input: &str) -> i64 {
-    let grid = input
-        .lines()
-        .map(|line| line.chars().collect::<Vec<_>>())
-        .collect::<Vec<_>>();
-    let dimensions = (grid.len() as i64, grid[0].len() as i64);
-
-    iproduct!(0..dimensions.0, 0..dimensions.1)
+fn part_two(grid: &[Vec<char>], (rows, cols): (i64, i64)) -> i64 {
+    iproduct!(0..rows, 0..cols)
         .filter(|(row, col)| {
             let (row, col) = (*row, *col);
-            (search_for(&grid, dimensions, (row, col), (1, 1), "MAS")
-                || search_for(&grid, dimensions, (row, col), (1, 1), "SAM"))
-                && (search_for(&grid, dimensions, (row + 2, col), (-1, 1), "MAS")
-                    || search_for(&grid, dimensions, (row + 2, col), (-1, 1), "SAM"))
+            (search_for(grid, (row, col), (1, 1), "MAS")
+                || search_for(grid, (row, col), (1, 1), "SAM"))
+                && (search_for(grid, (row + 2, col), (-1, 1), "MAS")
+                    || search_for(grid, (row + 2, col), (-1, 1), "SAM"))
         })
         .count() as i64
 }
